77163e6ec97dd67fa30ecbb634ce6a53225a5671
Kristjan Jansen <kristjan.jansen@gmail.com>
2014-11-13 16:02:11 +0200

    updates

--- README.md
+++ README.md
@@ -1,3 +1,33 @@
-### About
+### Templated
 
-siseveeb.ee prototype based on Bootstrap theming and Handlebars templates
+**/public/templates**
+
+[HandlebarsJS](http://handlebarsjs.com/) formaadis templatefailid. Nende kasutamine
+on jälgitav **/public/js/app.js** failist.
+
+### Leheküljed
+
+**/public/pages**
+
+Lehekülgede struktuurifailid [YAML](http://www.yaml.org/spec/1.2/spec.html) formaadis, mille alusel genereeritakse templatede abil HTML fail. Valitavad URL ribalt **?theme=insert-pagename-here** parameetriga. Kui parameetrit pole,
+kasutatakse **/public/pages/front.yml** faili.
+
+Leheküljefaile saab ise juurde teha ja URL parameetriga katsetada.
+
+### Themed
+
+**/themes**
+
+[SASS](http://sass-lang.com/guide) (.scss) formaadis themefailid mille alusel genereeritakse **/public/themes** olevad CSS themefailid. Kõik themed impordivad **themes/default.scss** faili, mis omakorda impordib kõik Bootstrapi stiilid ning millele lisatakse mõned muutujate ülekirjutused.
+
+**/public/themes**
+
+Genereeritud CSS themefailid, valitavad URL ribalt **?theme=insert-themename-here*** parameetriga. Asendavad täielikult Bootstrapi CSSi. Kui parameetrit pole, kasutatakse **/public/themes/blue.css** faili.
+
+**/generate-themes.sh**
+
+SASS &rarr; CSS generaator, vajab eelnevalt [NodeJS installeerimist](http://nodejs.org/download) ning käsurealt järgneva sisestamist:
+
+    npm install
+    ./generate-themes.sh
+
--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,11 @@
+// Set the theme CSS file based on query parameter
+
+var css = (urlParameter('theme') || 'blue')
+$('.theme-css').attr('href', './themes/' + css + '.css')
+
+
+// Load and compile templates
+
 $.ajaxSetup({async: false, cache: false})
 
 var tplKeys = [
@@ -25,36 +33,44 @@ tplKeys.forEach(function(key) {
 })
 
 
+// Load the page file based on URL parameter
+
 $.ajaxSetup({async: true})
 
-$.get('./data/test.yml', function(data) {
+var page = (urlParameter('page') || 'default')
+
+$.get('./pages/' + page + '.yml', function(data) {
  
     data = yaml.load(data)
-console.log(data)
-      $('body').html(tpl.wrapper({
-        nav: tpl.nav({data: data}),
-        sidebar: tpl.sidebar({data: data}),
-        main: tpl.main({
+
+    // Generate the HTML based on templates
+
+    $('body').html(tpl.wrapper({
+      nav: tpl.nav({data: data}),
+      sidebar: tpl.sidebar({data: data}),
+      main: tpl.main({
+        data: data,
+        alert: tpl.alert({data: data}),
+        filters: tpl.filters({data: data}),
+        exports: tpl.exports({data: data}),
+        config: tpl.config({data: data}),
+        actions_bottom: tpl.actions_bottom({data: data}),
+        actions_top: tpl.actions_top({data: data}),
+        table: tpl.table({
           data: data,
-          alert: tpl.alert({data: data}),
-          filters: tpl.filters({data: data}),
-          exports: tpl.exports({data: data}),
-          config: tpl.config({data: data}),
-          actions_bottom: tpl.actions_bottom({data: data}),
-          actions_top: tpl.actions_top({data: data}),
-          table: tpl.table({
-            data: data,
-            table_actions: tpl.table_actions({data: data})
-          }),
+          table_actions: tpl.table_actions({data: data})
         }),
-        modal: tpl.modal({
-          data: data,
-          actions_bottom: tpl.actions_bottom({data: data})
-        })
+      }),
+      modal: tpl.modal({
+        data: data,
+        actions_bottom: tpl.actions_bottom({data: data})
+      })
 
     }))
 
-  //  $("table").tablesorter();
+    // Launch event handlers
+
+    $("table").tablesorter();
 
     $('.table-actions .btn, .modal .btn').on('click', function(e) {
 
@@ -63,4 +79,21 @@ console.log(data)
 
     })
 })
+
+
+
+
+// Parse the URL parameters
+
+function urlParameter(param) {
+  var query = window.location.search.substring(1);
+  var vars = query.split("&");
+  for (var i = 0; i < vars.length; i++) {
+    var pair = vars[i].split("=");
+    if (pair[0] == param) {
+      return pair[1];
+    }
+  }
+  return(false);
+}
 
