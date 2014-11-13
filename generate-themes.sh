6a0dc670826f99016d88bce88ebd9b721cd6aa79
Kristjan Jansen <kristjan.jansen@gmail.com>
2014-11-13 15:14:01 +0200

    latest

--- generate-themes.sh
+++ generate-themes.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-./node_modules/.bin/node-sass themes/default.scss public/themes/default.css
+./node_modules/.bin/node-sass themes/blue.scss public/themes/blue.css
+./node_modules/.bin/node-sass themes/green.scss public/themes/green.css
+./node_modules/.bin/node-sass themes/orange.scss public/themes/orange.css
+./node_modules/.bin/node-sass themes/gray.scss public/themes/gray.css
